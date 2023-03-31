37c78aa18265d035888c1c40da64b37f3c57fc87
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2023-03-31 15:31:01 -0700

    Add initial tf/tg config for pr based deployments of frontend

--- client/terraform/variables.tf
+++ client/terraform/variables.tf
@@ -11,7 +11,6 @@ variable "target_env" {
 variable "frontend_build_path" {
   description = "Path to frontend build files"
   type        = string
-  default     = "frontend_build"
   validation {
     condition     = length(fileset(var.frontend_build_path, "**")) > 0
     error_message = "The frontend build path must contain at least one file."
